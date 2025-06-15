3265dc2e33df2e07b6edd0b4424b15e32b50a69a
Ibrohim Raufjonov <ibrohimraufjonov@Ibrohims-Laptop.local>
2025-06-15 20:47:20 +0500

    project has been created

---
+++ android/app/src/main/kotlin/com/ajmal/taxi_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ajmal.taxi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(TaxiApp());
+}
+
+class TaxiApp extends StatelessWidget {
+  const TaxiApp({super.key});
+
+
+  // ! themes style will be added here
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
